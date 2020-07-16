b7c3d57a6fe518a09586e6b10a44c9da6c048d55
gkarumbi <gkarumbi@gmail.com>
2020-07-16 22:40:17 +0300

    Add function to create our pitch object

---
+++ app/request.py
@@ -0,0 +1,13 @@
+from .models import Pitch
+
+
+def create_pitch():
+
+    pitch_objects = []
+    pit_1 = Pitch('A.I & Machine Learning', 'IDEA: The corona virus cure',"The cure works as follows : by engulfing the virus it completely takes over and destroys it", 'George Karumbi')
+    pit_2 = Pitch('A.I & Machine Learning', 'IDEA: The corona virus cure',"The cure works as follows : by engulfing the virus it completely takes over and destroys it", 'George Karumbi')
+    pitch_objects.append(pit_1,pit_2)
+
+    return pitch_objects
+
+
