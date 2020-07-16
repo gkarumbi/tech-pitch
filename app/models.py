61662977374996499dbecf5a0e3e6197d1642660
gkarumbi <gkarumbi@gmail.com>
2020-07-16 22:12:07 +0300

    Add pitch class to create our pitch objects

---
+++ app/models.py
@@ -0,0 +1,10 @@
+class Pitch:
+    '''
+    Class to create our pitch objects
+    '''
+
+    def __init__(self, category, title,idea,owner):
+        self.category = category
+        self.title = title
+        self.idea = idea
+        self.owner = owner
