2e41f74c45697a2bcb7f2fd9d3bb52cfea60f6b3
gkarumbi <gkarumbi@gmail.com>
2020-07-18 13:54:04 +0300

    Add function to define the logout route

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,13 +1,9 @@
 from flask import render_template,redirect,url_for, flash,request
-from flask_login import login_user
+from flask_login import login_user,logout_user,login_required
 from ..models import User
 from .forms import RegistrationForm,LoginForm
 from .. import db
 from . import auth
-
-@auth.route('/login')
-def login():
-    return render_template('auth/login.html')
 
 @auth.route('/register',methods = ["GET","POST"])
 def register():
@@ -31,7 +27,14 @@ def login():
 
         flash('Invalid username or Password')
 
-    title = "watchlist login"
+    title = "PitchUp|login"
     return render_template('auth/login.html',login_form = login_form,title=title)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
 
 
