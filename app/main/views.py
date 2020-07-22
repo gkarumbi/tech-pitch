1f2e4c8c86ed0640274fea36e3121668882cb8ee
gkarumbi <gkarumbi@gmail.com>
2020-07-22 20:12:27 +0300

    Add route to new pitch form

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,9 @@
 from flask import render_template, request, redirect, url_for,abort
 from . import main
-from .forms import PostPitch
+from .forms import PostPitch, CategoryForm. CommentForm
+from  ..import db
 
-from ..request import create_pitch
-from ..models import Pitch,User
+from ..models import Pitch,User,Comments,Category
 
 #Views 
 @main.route('/')
@@ -12,38 +12,46 @@ def index():
     '''
     View functions that displays the home page
     '''
-    title = 'Welcome to largest business networking site'
-    pitch_in_views = create_pitch()
+    title = 'Welcome to largest repositories of pitches'
+    categories =Category.get_categories()
+    pitches = Pitch.query.order_by('id').all()
+    print(pitches)
 
-    return render_template('index.html', title = title,pitch_in_html = pitch_in_views)
+    return render_template('index.html', pitches=pitches, categories = categories)
 
-@main.route('/post/<pitch_url>',methods = ['GET','POST'])
 
-def pitch_page(pitch_url):
+#Route for new Pitch
+
+@main.route('/post/new-pitch/<int:id>',methods = ['GET','POST'])
+@login_required
+def pitch_page(id):
 
     form = PostPitch()
-
+    category = Category.query.filter_by(id=id).first()
     '''
     Function to render page that displays individual page for the pitch
     '''
     title = 'Welcome to largest business networking site'
-    pitch = create_pitch()
-    print(pitch)
+    
+    if category is None:
+        abort(404)
     if form.validate_on_submit():
         
-        #category =form.category.data
-        pitch = form.pitch.data
-        new_pitch = Pitch(pitch,pitch,pitch,'George Karumbi')
+        pitch =form.pitch.data
+        new_pitch = Pitch(pitch=pitch,category_id=category.id,user_id = user.id)
         print(new_pitch)
         new_pitch.save_pitches()
-        return redirect(url_for('timeline',pitch_url = pitch.title ))
-    return render_template('pitch.html',pitch = pitch,pitch_form=form)
+        return redirect(url_for('.category',id= category_id.id ))
+    return render_template('pitch.html',category = category,pitch_form=form)
 
-@main.route('/user/<uname>')
-def profile(uname):
-    user = User.query.filter_by(username = uname).first()
 
-    if user is None:
-        abort(404)
+    
 
-    return render_template("profile/profile.html", user = user)
+# @main.route('/user/<uname>')
+# def profile(uname):
+#     user = User.query.filter_by(username = uname).first()
+
+#     if user is None:
+#         abort(404)
+
+#     return render_template("profile/profile.html", user = user)
