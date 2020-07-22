8dd9bbb8fc9e16d498939ab52e62fa32fa0af290
gkarumbi <gkarumbi@gmail.com>
2020-07-22 22:05:35 +0300

    Add route for posting a new comment

--- app/main/views.py
+++ app/main/views.py
@@ -67,10 +67,52 @@ def new_category():
 
     if form.validate_on_submit():
         name = form.name.data
-        new_category = PitchCategory(name = name)
+        new_category = Category(name = name)
         new_category.save_category()
 
         return redirect(url_for('.index'))
 
     title = 'New category'
     return render_template('new_category.html', category_form = form, title = title)
+
+#Route to view single pitch plus its comments
+@main.route('/view-pitch/<int:id>', methods=['GET', 'POST'])
+@login_required
+def view_pitch(id):
+    """
+    Function the returns a single pitch with comment section
+    """
+    category = Category.get_categories()
+    pitches = Pitch.query.get(id)
+    
+
+    if pitches is None:
+        abort(404)
+    
+    comment = Comments.get_comments(id)
+    
+    return render_template('view-pitch.html', pitches = pitches, comment = comment, category_id = id, categories=category)
+
+
+#Rout to add a new comment
+@main.route('/new-comment/<int:id>', methods=['GET', 'POST'])
+@login_required
+def post_comment(id):
+    """ 
+    Function to post new comments on our pitches
+    """
+    
+    form = CommentForm()
+    title = 'post comment'
+    pitches = Pitch.query.filter_by(id=id).first()
+
+    if pitches is None:
+        abort(404)
+
+    if form.validate_on_submit():
+        comment = form.comment.data
+        new_comment = Comments(comment = comment, user_id = current_user.id, pitches_id = pitches.id)
+        new_comment.save_comments()
+        return redirect(url_for('.view_pitch', id = pitches.id))
+
+    return render_template('new-comment.html', comment_form = form, title = title)
